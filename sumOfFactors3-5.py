fae079b41a01c6071a61bd09e5ccfcf91f96cad3
jham20x6 <78266491+jham20x6@users.noreply.github.com>
2025-01-01 15:08:09 -0800

    Update sumOfFactors3-5.py
    Euler says All factors below 1000.

--- sumOfFactors3-5.py
+++ sumOfFactors3-5.py
@@ -2,8 +2,8 @@ import sys
 
 sum = 0;
 
-#for i in range(1,(int(sys.argv[1])+1)) :
-for i in range(int(sys.argv[1])+1) :
+#for i in range(1,(int(sys.argv[1]))) :
+for i in range(int(sys.argv[1])) :
 		if (i % 5 == 0 or i % 3 == 0) :
 				sum+=i
 print(sum)
