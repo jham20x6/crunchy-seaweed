dc80ed9d6a54350e2621eebb32be04e597fc6e43
Jeff Hamlin <jeff.m.hamlin@gmail.com>
2025-02-03 11:00:06 -0800

    optimizing python with c lol

--- lpf.c
+++ lpf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
 
+// returns whether the number num is prime
+// by returning 0 if no factors are found
+// returns (halts at) 1 if a factor is found,
+// aside from num and 1.  Starts looking at 2.
 int has_factors(uint64_t num) {
   int num_factors = 0;
 
@@ -14,22 +19,44 @@ int has_factors(uint64_t num) {
   return num_factors;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     uint64_t n = 0;
     
-    scanf("%ld", &n);
-    
-    for (uint64_t i = n; i > 2; i--) {
-        if(has_factors(i) == 0) {
+    //scanf("%ld", &n);
+    n = strtoll(argv[1],NULL,10);
+
+// find the largest prime factor of n   
+/*    for (uint64_t i = n; i > 2; i--) {
+        if(!has_factors(i)) {
             printf(" %ld", i);
             printf("\n");
             fflush(stdout);
             return 0;
         }
     }
-    printf("\n");
+*/
+    int64_t int_sqrt = ((int64_t) sqrt((double)n));
+    int64_t highestPrimeDivisor = 1;
+    int64_t quotient = 0;
+    for (int64_t divisor = int_sqrt; divisor > 2; divisor--) {
+        if (n % divisor == 0) {
+            quotient = (int64_t) (n/divisor);
+            if (!has_factors(quotient)) {
+            	printf(" %ld\n", quotient);
+                fflush(stdout);
+				return 0;
+            }
+            if (!has_factors(divisor)) {
+                if (highestPrimeDivisor == 1) {
+                    highestPrimeDivisor = divisor;
+				}
+            }
+        }
+    }
+    if (highestPrimeDivisor == 1) { highestPrimeDivisor = n ; }
+    printf(" %ld\n", highestPrimeDivisor );
     fflush(stdout);
 
-  return 0;
+    return 0;
 }
